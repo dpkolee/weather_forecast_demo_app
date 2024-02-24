52216a7fa1a5981bc5989952ec9ac6d5a13a7574
dpkolee <deepakolee@gmail.com>
2024-02-25 00:07:54 +0545

    Feat(styling): - tailwind css added for styling along with configurations.

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,6 @@
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
+</script>
 <template>
   <RouterView />
 </template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,15 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
 <template>
   <main>
-    <h1>This is a homepage </h1>
+    <nav>
+      <router-link to="/"> Home </router-link> |
+      <router-link to="/sign-up"> Sign Up </router-link> |
+      <router-link to="/sign-in"> Login </router-link> |
+    </nav>
+    <p class="text-2xl xl:text-3xl font-extrabold">This is a Homepage</p>
+    <router-view />
   </main>
 </template>
